799b96783f9e07588ba3984536631a5e4ad2e32d
윤해빈 <ywy2001@naver.com>
2022-11-11 17:53:39 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("-----      ------          -----    -----");
+        System.out.println("|    |     |    |          |    |   |    |");
+        System.out.println("|    | --- |    |          |    | - |    |");
+        System.out.println("|               |          |             |");
+        System.out.println("|    ^      ^   |          |  o       o  |");
+        System.out.println("|               |          |             |");
+        System.out.println("-----------------          ---------------");
+
+        System.out.println("\t\t\tHELLO DANKOOK UNIV\t");
+        System.out.println("\t\t\tNIGHT OF DEVELOPER\t");
+    }
+}
